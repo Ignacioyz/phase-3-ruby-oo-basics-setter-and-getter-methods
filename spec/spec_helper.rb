35b4e7906020917750c5c31cffe45514b884226e
Ignacio Yz <ignacioyz@gmail.com>
2022-06-30 14:25:08 -0400

    commit tree b0e389bc8ef350fd3e9aeb2aae22a64c88543fc8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,39 @@ require_relative '../lib/person'
 RSpec.configure do |config|
   config.order = :default
 end
+
+class Dog 
+   
+  def name=(name)
+    @name = name
+    
+  end
+
+  def name 
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  
+  end 
+  def breed 
+    @breed
+  end
+end 
+d1 = Dog.new()
+
+class Person 
+  def name=(name)
+    @name = name 
+  end 
+  def name 
+    @name 
+  end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job 
+  end
+end
+
